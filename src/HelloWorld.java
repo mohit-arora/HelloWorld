f4cac5cf8104cdc400b2870781aea457bdc903c1
vinayzopper <vinay.kumar@zopper.com>
2016-01-25 17:26:35 +0530

    hh

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,14 @@ public class HelloWorld {
 
 	public static void main(String args[]) {
 		System.out.println("Hello World ");
-	}			
+		
+		
+		
+	}	
+	
+	public String getname()
+	{
+		return "vin";
+	}
+
 }
